eef8e1113e513e050eb5184ccd95063e98553897
Hanamoto-masa <hanamoto.masahiko0113@gmail.com>
2020-02-08 21:34:19 +0900

    ゲームループ

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// ゲームスピード
+const GAME_SPEED = 1000/60;
+
 // 画面サイズ
 const SCREEN_W = 180;
 const SCREEN_H = 320;
@@ -19,6 +22,17 @@ let con = can.getContext("2d");
 can.width = CANVAS_W;
 can.height = CANVAS_H;
 
+// フィールド（仮想画面）
+let vcan = document.createElement("canvas");
+let vcon = vcan.getContext("2d");
+vcan.width  = FIELD_W;
+vcan.height = FIELD_H;
+
+// カメラの座標
+let camera_x = 0;
+let camera_y = 0;
+
+
 function rand(min,max){
   return Math.floor(Math.random()*(max-min+1))+min;
 }
@@ -26,16 +40,21 @@ function rand(min,max){
 // 星
 class Star{
   constructor(){
-    this.x = rand(0,FIELD_W)<<8;
-    this.y = rand(0,FIELD_H)<<8;
+    this.x  = rand(0,FIELD_W)<<8;
+    this.y  = rand(0,FIELD_H)<<8;
     this.vx = 0;
     this.vy = rand(30,200);
     this.sz = rand(1,2);
   }
 
   draw(){
-    con.fillStyle = rand(0,2)!=0?"66f":"#8af";
-    con.fillRect(this.x>>8,this.y>>8,this.sz,this.sz);
+    let x = this.x>>8;
+    let y = this.y>>8;
+    if(x<camera_x || x>=camera_x+SCREEN_W
+       ||y<camera_y || y>=camera_x+SCREEN_H)return;
+
+    vcon.fillStyle = rand(0,2)!=0?"66f":"#8af";
+    vcon.fillRect(x,y,this.sz,this.sz);
 
   }
 
@@ -52,7 +71,24 @@ class Star{
 let star = [];
 for(let i = 0;i<STAR_MAX;i++)star[i] = new Star();
 
-con.fillStyle = "black";
-con.fillRect(0,0,SCREEN_W,SCREEN_H);
 
-for(let i = 0;i<STAR_MAX;i++)star[i].draw();
+
+setInterval(gameLoop,GAME_SPEED);
+
+// ゲームループ
+function gameLoop(){
+
+  // 移動の処理
+  for(let i = 0;i<STAR_MAX;i++)star[i].update();
+
+  // 　描画の処理
+  vcon.fillStyle = "black";
+  vcon.fillRect(0,0,SCREEN_W,SCREEN_H);
+
+  for(let i = 0;i<STAR_MAX;i++)star[i].draw();
+
+  // 仮想画面から実際のキャンバスにコピー
+  con.drawImage(vcan,camera_x,camera_y,SCREEN_W,SCREEN_H,
+    0,0,CANVAS_W,CANVAS_H);
+
+}
